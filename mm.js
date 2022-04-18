9c2709988950ae2ef0cb5a89b2efd25a6e6fde52
Lihach.ok <Lihach.ok@gmail.com>
2022-04-18 19:59:03 +0300

    add chart

---
+++ mm.js
@@ -0,0 +1,22 @@
+if (typeof window.ethereum !== 'undefined') {
+    console.log('MetaMask is installed!');
+
+    const ethereumButton = document.querySelector('.enableEthereumButton');
+    const showAccount = document.querySelector('.showAccount');
+
+    ethereumButton.addEventListener('click', () => {
+        getAccount();
+    });
+
+    async function getAccount() {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        showAccount.innerHTML = account;
+        console.log("connection:",  ethereum.isConnected())
+    }
+}else {
+
+} console.log('MetaMask is not installed!');
+
+
+
