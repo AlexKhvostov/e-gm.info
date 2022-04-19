c4d16c345bec80504db5e0ddccae4672c5410cb7
Lihach.ok <Lihach.ok@gmail.com>
2022-04-19 09:23:26 +0300

    edit html

--- assets/dist/js/DashBoard.js
+++ assets/dist/js/DashBoard.js
@@ -1,15 +1,11 @@
 
 
-// // const response = fetch("http://flask.hvostov.by/tableInfo");
-// // const response = fetch("https://expressgameapp.herokuapp.com/tableinfo");
-// const response = fetch("https://expressgameapp.herokuapp.com/",  {
-//     method: 'GET',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify({ name: 'Ilija'})
-// });
-// response.then((response) => {
-//     return response.json();
-// })
-//     .then((data) => {
-//         console.log(data);
-//     });
+// const response = fetch("http://flask.hvostov.by/tableInfo");
+const response = fetch("https://expressgameapp.herokuapp.com/tableinfo");
+// const response = fetch("https://expressgameapp.herokuapp.com/");
+response.then((response) => {
+    return response.json();
+})
+    .then((data) => {
+        console.log(data);
+    });
