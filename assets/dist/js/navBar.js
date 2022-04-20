d04cfeebb0896d4be633636f822ee557feb6eb9a
Lihach.ok <Lihach.ok@gmail.com>
2022-04-20 04:49:24 +0300

    edit dashboard

--- assets/dist/js/navBar.js
+++ assets/dist/js/navBar.js
@@ -1,18 +1,17 @@
-$(function() {
-    let header = $('.nav');
-    let hederHeight = header.height(); // вычисляем высоту шапки
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
-    $(window).scroll(function() {
-        if($(this).scrollTop() > 1) {
-            header.addClass('header_fixed');
-            $('body').css({
-                'paddingTop': hederHeight+'px' // делаем отступ у body, равный высоте шапки
-            });
-        } else {
-            header.removeClass('header_fixed');
-            $('body').css({
-                'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
-            })
-        }
-    });
-});
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
